b4bbb3407b739a26f7d70aa1e391137f7d1b2c7e
Michaela Meissner <m.p.meissner85@gmail.com>
2023-07-30 17:43:46 -0400

    Added URLpatterns to URLs.py

---
+++ backend/petprofiles/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from petprofiles import views 
+from django.conf import settings 
+from django.conf.urls.static import static 
+
+
+urlpatterns = [
+    path('', views.get_all_profiles),
+    path('<int:pk>/', views.pet_single),
+    path('createpet/', views.create_pet),
+    path('changepet/', views.change_pet),
+    path('images/<int:pk>/', views.get_images),
+    path('guest/', views.guest_views),
+]
